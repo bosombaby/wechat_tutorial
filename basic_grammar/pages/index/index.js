b9dacd07b13ef9e47414b7b7a27dc484a08e76fc
bosom <1245924849@qq.com>
2024-06-17 18:08:06 +0800

    feat: 基本配置

---
+++ basic_grammar/pages/index/index.js
@@ -0,0 +1,24 @@
+// index.js
+const defaultAvatarUrl =
+  "https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0";
+
+Page({
+  data: {
+    title: "【微信小程序培训】",
+    courseList: [
+      {
+        key: "template_render_grammar",
+        name: "模板渲染语法",
+        link: "/pages/template_render/template_render",
+      },
+      {},
+    ],
+  },
+  goLink(e) {
+    const link = e.currentTarget.dataset.link;
+    console.log("链接跳转", link);
+    wx.reLaunch({
+      url: link,
+    });
+  },
+});
