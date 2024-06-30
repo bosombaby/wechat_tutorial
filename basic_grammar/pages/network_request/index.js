de13bf304bfc23d4b20f1d3acca858bfcd4c271a
bosom <1245924849@qq.com>
2024-06-30 17:05:54 +0800

    feat: 网络请求

--- basic_grammar/pages/network_request/index.js
+++ basic_grammar/pages/network_request/index.js
@@ -1,66 +1,34 @@
 // pages/network_reuest/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  getSystemId(){
+    wx.login({
+      success (res) {
+        if (res.code) {
+          console.log('code:', res.code)
+          //发起网络请求
+          wx.request({
+            url: 'https://api.weixin.qq.com/sns/jscode2session',
+            data: {
+              appid:'wxee24caea5cae426a', 
+              secret:'5175859809cc0cae756213dbf1106e11', 
+              js_code:res.code,
+              grant_type:'authorization_code' 
+            },
+            success: res => {
+              if (res.data.openid) {
+                console.log('成功获取openid:', res.data.openid); // 成功获取到openid
+              } else {
+                console.error('获取openid失败:', res.data.errmsg); // 没有获取到openid，返回错误信息
+              }
+            },
+            fail: err => {
+              console.error('请求失败:', err.errMsg); // 请求失败，返回错误信息
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
   }
 })
